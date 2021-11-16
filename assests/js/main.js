742c746da9e03eff630213335debcb0adc0f966b
Vignesh Lokeshbabu <websoldesk@google.com>
2021-11-16 18:01:34 +0530

    1) form validation with a bug, 2) data posting is working fine.

--- assests/js/main.js
+++ assests/js/main.js
@@ -8,34 +8,82 @@ const message = document.getElementById("message");
 const form = document.getElementById("contactform");
 const errormessage = document.getElementById("errormsg");
 
-form.addEventListener("submit", (e) => {
-    let errmsg = [];
+// form.addEventListener("submit", (e) => {
+//     let errmsg = [];
+//     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//     var validmail = email.value.match(mailformat);
+
+//     if(fullname.value === '' || fullname.value === null) {
+//         errmsg.push("Please enter your NAME...:(");
+//     }
+
+//     if(email.value === '' || email.value === null) {
+//         errmsg.push("Please enter your EMAIL ID...:(");
+//     } else {
+//         if(!validmail) {
+//             errmsg.push("Please enter a VALID EMAIL ID...:(");
+//         }
+//     }
+
+//     if(message.value === '' || message.value === null) {
+//         errmsg.push("Please enter your MESSAGE...:(");
+//     }
+
+//     if(errmsg.length > 0) {
+//         e.preventDefault();
+//         errormessage.innerHTML = errmsg.join(", ");
+//     } else {
+//         // return true;
+//     }
+// })
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    checkInputs();
+    sendMessage();
+})
+
+function checkInputs() {
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     var validmail = email.value.match(mailformat);
 
-    if(fullname.value === '' || fullname.value === null) {
-        errmsg.push("Please enter your NAME...:(");
+    // for trimming the whitespaces
+    const fullNameValue = fullname.value.trim();
+    const emailValue = email.value.trim();
+    const messageValue = message.value.trim();
+
+    if (fullNameValue === '' || fullNameValue === null) {
+        setErrorFor(fullname, 'Name Can\'t be empty.')
+        form.reset() = false;
     }
 
-    if(email.value === '' || email.value === null) {
-        errmsg.push("Please enter your EMAIL ID...:(");
+    if (emailValue === '' || emailValue === null) {
+        setErrorFor(email, 'Email can\'t be empty.');
+        form.reset() = false;
     } else {
-        if(!validmail) {
-            errmsg.push("Please enter a VALID EMAIL ID...:(");
+        if (!validmail) {
+            setErrorFor(email,'Please enter a valid Email ID.');
+            form.reset() = false;
         }
     }
 
-    if(message.value === '' || message.value === null) {
-        errmsg.push("Please enter your MESSAGE...:(");
+    if (messageValue === '' || messageValue === null) {
+        setErrorFor(message, 'Please enter a Message.')
+        form.reset() = false;
     }
+}
 
-    if(errmsg.length > 0) {
-        e.preventDefault();
-        errormessage.innerHTML = errmsg.join(", ");
-    } else {
-        // return true;
-    }
-})
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-element error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-element success';
+}
 
 // Success & Error Messages Section 
 
@@ -56,12 +104,10 @@ function error() {
 // Sending Message from Form to Mail ID Section
 
 function sendMessage() {
-    console.log("This function is working");
     var name = $('#name').val();
     var email = $('#email').val();
     var message = $('#message').val();
     var body = 'Name: ' + name + '<br>Email: ' + email + '<br>Message: ' + message;
-    console.log(name, email, message);
 
     Email.send({
         SecureToken: "02a53baa-a290-44cf-b2d2-809c706d3f13",
