7f1f94b8706a7d2c438a0b3b909f68d1819136e4
Vignesh Lokeshbabu <websoldesk@google.com>
2021-12-20 09:18:10 +0530

    Completed project section for desktop view

--- assests/js/main.js
+++ assests/js/main.js
@@ -156,16 +156,9 @@ const modal = document.querySelector('#modal');
 const mimModalBtn = document.querySelector('#mim-modal-btn');
 const mimStudPortalModalBtn = document.querySelector('#mim-stud-portal-modal-btn');
 const goVeganModalBtn = document.querySelector('#go-vegan-modal-btn');
-const closeIcon = document.querySelector('#close');
-const closeBtn = document.querySelector('#close-btn');
-const img = document.querySelector("#sample-image");
 
-mimModalBtn.addEventListener('click', () =>{
+mimModalBtn.addEventListener('click', () => {
     modal.style.display = 'block';
-    // var img = document.getElementById("sample-image");
-    // var others = document.getElementById("other-details");
-    // img.innerHTML="<img scr=\"../images/Screenshot (22).png\" alt=\"Image2\" width=\"100px\" height=\"100px\" style=\"border: 1px solid black; \">"
-    // img.appendChild(document.createElement("img")).src="../images/Screenshot (21).png";
     document.getElementById("project-title").innerHTML = "MEASI Institute of Management";
     document.getElementById("description").innerHTML = "MEASI Institute of Management is a real-time website for our college, which was proposed by the management of our institute. Under the guidance of our faculty members and Branding Guidelines provided by the management, we students (a team of 5 members) partitioned the website into modules and implemented the project using HTML, CSS, JavaScript along with Bootstrap Template. <br><br> <span style=\"margin-left: 25px;\"></span>In this project, I was chosen as the team leader for the project team. The result for my part of work is the Homepage, Admission Module and PRIME module. And I brought in the responsiveness wherever required and helped my fellow team-mates to complete the tasks.";
     document.getElementById("tech-used").innerHTML = "HTML, CSS, JavaScript, BootStrap Templates.";
@@ -174,11 +167,8 @@ mimModalBtn.addEventListener('click', () =>{
     document.getElementById("link").innerHTML = "Not available... :(";
 })
 
-mimStudPortalModalBtn.addEventListener('click', () =>{
+mimStudPortalModalBtn.addEventListener('click', () => {
     modal.style.display = 'block';
-    // var img = document.getElementById("sample-image");
-    // img.innerHTML="<img scr=\"../images/Screenshot (21).png \" alt=\"Image3\" width=\"100px\" height=\"100px\" style=\"border: 1px solid black; \">"
-    // img.innerHTML="<img scr=\"../images/Screenshot (21).png \" alt=\"Image4\" width=\"100px\" height=\"100px\" style=\"border: 1px solid black; \">"
     document.getElementById("project-title").innerHTML = "MIM Student Portal"
     document.getElementById("description").innerHTML = "MEASI Institute of Management (MIM) Student Portal is a real-time Student Management Portal that is primarily an extension of the MIM website responsible for the digital handling process of the academic and co-curricular marks and scores of all the students. This is a role-based system which allows all the/specific privileges to a user as per the role of the respective user. <br><br> <span style=\"margin-left: 25px;\"></span> This project was proposed by the management, and a team of 7 students where chosen for which I was the team leader again and provided with the SRS and Branding Guidelines. The team was further splitted into Frontend and Backend teams. <br><br> <span style=\"margin-left: 25px;\"></span> In this project, I dealt with Frontend. I, along with other fellow team-mates used React.JS for the UI part of the web-app.";
     document.getElementById("tech-used").innerHTML = "MERN Stack";
@@ -187,11 +177,8 @@ mimStudPortalModalBtn.addEventListener('click', () =>{
     document.getElementById("link").innerHTML = "Not available... :(";
 })
 
-goVeganModalBtn.addEventListener('click', () =>{
+goVeganModalBtn.addEventListener('click', () => {
     modal.style.display = 'block';
-    // var img = document.getElementById("sample-image");
-    // img.innerHTML="<img scr=\"../images/Screenshot (21).png \" alt=\"Image3\" width=\"100px\" height=\"100px\" style=\"border: 1px solid black; \">"
-    // img.innerHTML="<img scr=\"../images/Screenshot (21).png \" alt=\"Image4\" width=\"100px\" height=\"100px\" style=\"border: 1px solid black; \">"
     document.getElementById("project-title").innerHTML = "GoVegan"
     document.getElementById("description").innerHTML = "GoVegan is a blog website, which I developed out of my own interest using the ThemeForest Canvas HTML Template to bring the awareness on the benefits of turning vegan over non-veganism.";
     document.getElementById("tech-used").innerHTML = "HTML Template";
@@ -200,55 +187,6 @@ goVeganModalBtn.addEventListener('click', () =>{
     document.getElementById("link").innerHTML = "Yet to update";
 })
 
-closeIcon.addEventListener('click', closeModal);
-closeBtn.addEventListener('click', closeModal);
-window.addEventListener('click', outSideClick);
-
 function closeModal() { 
     modal.style.display = 'none';
 }
-
-function outSideClick(e) {
-    if( e.target === modal ) {
-        modal.style.display = 'none';
-    }
-}
-
-
-
-// $(document).ready(function() {
-//     $('#autoWidth').lightSlider({
-//         autoWidth:true,
-//         loop:true,
-//         onSliderLoad: function() {
-//             $('#autoWidth').removeClass('cS-hidden');
-//         } 
-//     });  
-//   });
-
-// $(document).ready(function() {
-//     $('#responsive').lightSlider({
-//         item:4,
-//         loop:false,
-//         slideMove:2,
-//         easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-//         speed:600,
-//         responsive : [
-//             {
-//                 breakpoint:800,
-//                 settings: {
-//                     item:3,
-//                     slideMove:1,
-//                     slideMargin:6,
-//                   }
-//             },
-//             {
-//                 breakpoint:480,
-//                 settings: {
-//                     item:2,
-//                     slideMove:1
-//                   }
-//             }
-//         ]
-//     });  
-// });
